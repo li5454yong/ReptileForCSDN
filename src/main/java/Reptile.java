ab5683da4c4f9733d47535a74cfc700edbea72ab
li5454yong <283056081@qq.com>
2017-01-26 20:52:24 +0800

    按照博客中下一篇url，遍历所有文章

--- src/main/java/Reptile.java
+++ src/main/java/Reptile.java
@@ -1,5 +1,3 @@
-import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.lang.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -10,7 +8,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by admin on 2017/1/20.
+ * CSDN博客备份工具
+ * Created by lxg on 2017/1/20.
  */
 public class Reptile {
 
@@ -18,8 +17,15 @@ public class Reptile {
     static Pattern p = null;
     static Matcher m = null;
 
-    public static void main(String[] args) throws IOException {
-        String url = "http://blog.csdn.net/u283056051/article/details/52187091";
+    /**
+     * 获取文章内容节点
+     *
+     * @param articleUrl
+     * @return
+     * @throws IOException
+     */
+    static Element getContentElement(String articleUrl) throws IOException {
+        String url = "http://blog.csdn.net" + articleUrl;
         Document doc = Jsoup.connect(url)
                 .header("Accept", "*/*")
                 .header("Accept-Encoding", "gzip, deflate")
@@ -28,31 +34,63 @@ public class Reptile {
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:48.0) Gecko/20100101 Firefox/48.0")
                 .timeout(5000)
                 .get();
-
         Element div = doc.getElementById("article_details");
 
+        return div;
+    }
+
+    /**
+     * 获取文章标题
+     *
+     * @param div
+     * @return
+     */
+    static String getTitle(Element div) {
         Elements span = div.select("span.link_title");
         Element a = span.get(0).child(0);//获取文章名称
         String title = a.html();
         System.out.println(a.html());
 
+        return title;
+    }
+
+    /**
+     * 获取文章发布时间
+     *
+     * @param div
+     * @return
+     */
+    static String getPostDate(Element div) {
         Elements postDate = div.select("span.link_postdate");
         String date = postDate.get(0).html();
-        date = Pattern.compile("-").matcher(date).replaceAll("/");
-        System.out.println(date);
 
+        return date;
+    }
 
+    /**
+     * 获取文章正文
+     *
+     * @param div
+     * @return
+     */
+    static String getAtricleContent(Element div) {
         Element atricle_content = div.getElementById("article_content");
+        String atricle = atricle_content.html();
+        return atricle;
+    }
 
+    /**
+     * 去除冗余的html标签
+     * @param atricle
+     * @return
+     */
+    static String replaceRedundancyElement(String atricle) {
 
-        String html = atricle_content.html();
-        //System.out.println(html);
-
+        String okContent = atricle;
         //去除js代码
         String regEx = "<script[^>]*?>[\\s\\S]*?<\\/script>";
         p = Pattern.compile(regEx);
-        m = p.matcher(html);
-        String okContent = null;
+        m = p.matcher(okContent);
         if (m.find()) {
             okContent = m.replaceAll("");
         }
@@ -60,26 +98,26 @@ public class Reptile {
         //去除div标签
         p = Pattern.compile("<div (.*?)>");
         m = p.matcher(okContent);
-        if(m.find()){
+        if (m.find()) {
             okContent = m.replaceAll("");
         }
         p = Pattern.compile("</div>");
         m = p.matcher(okContent);
 
-        if(m.find()){
+        if (m.find()) {
             okContent = m.replaceAll("");
         }
 
         //去除span标签
         p = Pattern.compile("<span (.*?)>");
         m = p.matcher(okContent);
-        if(m.find()){
+        if (m.find()) {
             okContent = m.replaceAll("");
         }
         p = Pattern.compile("</span>");
         m = p.matcher(okContent);
 
-        if(m.find()){
+        if (m.find()) {
             okContent = m.replaceAll("");
         }
 
@@ -87,41 +125,148 @@ public class Reptile {
         //去除p标签
         p = Pattern.compile("<p (.*?)>");
         m = p.matcher(okContent);
-        if(m.find()){
+        if (m.find()) {
             okContent = m.replaceAll("");
         }
         p = Pattern.compile("</p>");
         m = p.matcher(okContent);
 
-        if(m.find()){
+        if (m.find()) {
             okContent = m.replaceAll("");
         }
         p = Pattern.compile("<p>");
         m = p.matcher(okContent);
 
-        if(m.find()){
+        if (m.find()) {
             okContent = m.replaceAll("");
         }
 
         //去除span标签
         p = Pattern.compile("&lt;");
         m = p.matcher(okContent);
-        if(m.find()){
+        if (m.find()) {
             okContent = m.replaceAll("<");
         }
         p = Pattern.compile("&gt;");
         m = p.matcher(okContent);
 
-        if(m.find()){
+        if (m.find()) {
             okContent = m.replaceAll(">");
         }
 
-        System.out.println(okContent);
+        return okContent;
+    }
 
 
+    /**
+     * 处理正文中的特殊字符
+     * @param atricle
+     * @return
+     */
+    static String handlSpecialChar(String atricle){
+        p = Pattern.compile("\\$");
+        m = p.matcher(atricle);
+        if (m.find()) {
+            atricle = m.replaceAll("\\\\\\$");
+        }
 
-        FileInputStream fileInputStream = new FileInputStream("E:\\123.xml");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+        p = Pattern.compile("\\{");
+        m = p.matcher(atricle);
+        if (m.find()) {
+            atricle = m.replaceAll("\\\\\\{");
+        }
+
+        p = Pattern.compile("\\}");
+        m = p.matcher(atricle);
+        if (m.find()) {
+            atricle = m.replaceAll("\\\\\\}");
+        }
+
+        return atricle;
+    }
+
+    /**
+     * 处理文件名中的特殊字符
+     * @param title
+     * @return
+     */
+    static String handlSpecialCharForTitle(String title){
+        p = Pattern.compile("\\\\");
+        m = p.matcher(title);
+        if (m.find()) {
+            title = m.replaceAll("");
+        }
+
+        p = Pattern.compile("\\/");
+        m = p.matcher(title);
+        if (m.find()) {
+            title = m.replaceAll("");
+        }
+
+        p = Pattern.compile(":");
+        m = p.matcher(title);
+        if (m.find()) {
+            title = m.replaceAll("");
+        }
+
+        p = Pattern.compile("\\*");
+        m = p.matcher(title);
+        if (m.find()) {
+            title = m.replaceAll("");
+        }
+        p = Pattern.compile("\\?");
+        m = p.matcher(title);
+        if (m.find()) {
+            title = m.replaceAll("");
+        }
+        p = Pattern.compile("\\|");
+        m = p.matcher(title);
+        if (m.find()) {
+            title = m.replaceAll("");
+        }
+        p = Pattern.compile("<");
+        m = p.matcher(title);
+        if (m.find()) {
+            title = m.replaceAll("");
+        }
+        p = Pattern.compile(">");
+        m = p.matcher(title);
+        if (m.find()) {
+            title = m.replaceAll("");
+        }
+        p = Pattern.compile("\"");
+        m = p.matcher(title);
+        if (m.find()) {
+            title = m.replaceAll("");
+        }
+        return title;
+    }
+    /**
+     * 获取下一篇文章链接
+     * @param div
+     * @return
+     */
+    static String getNextHref(Element div){
+        Elements nextArticle = div.getElementsByClass("next_article");
+        if(nextArticle.size() == 0){
+            return null;
+        }
+        String nextHref = nextArticle.get(0).child(1).attr("href");
+
+        return nextHref;
+    }
+
+    /**
+     * 读取模板，生成文章
+     * @param title
+     * @param date
+     * @param okContent
+     * @param templatePath
+     * @throws Exception
+     */
+    static void create(String title, String date, String okContent, String templatePath) throws Exception {
+        FileInputStream fileInputStream = new FileInputStream(templatePath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream, "utf-8"));
         String line = reader.readLine(); // 读取第一行
         StringBuffer buffer = new StringBuffer();
         while (line != null) { // 如果 line 为空说明读完了
@@ -132,28 +277,66 @@ public class Reptile {
         reader.close();
         fileInputStream.close();
 
-        String page = replace(buffer.toString(),"title",title);
+        String str = new String(buffer.toString().getBytes("UTF-8"));
 
-        page = replace(page,"content",okContent);
+        String page = replace(str, "title", title);
+        page = replace(page, "post_date", date);
+        page = replace(page, "content", okContent);
 
-        page = replace(page,"post_date",date);
 
         RandomAccessFile randomFile = null;
-        randomFile = new RandomAccessFile("E:\\456.xml", "rw");
+        randomFile = new RandomAccessFile("E:\\CSDN\\" + handlSpecialCharForTitle(title) + ".xml", "rw");
         randomFile.seek(randomFile.length());
-        randomFile.write(page.getBytes());
+        randomFile.write(page.getBytes("UTF-8"));
 
         randomFile.close();
-
     }
 
-    static String replace(String str,String name,String content){
+    /**
+     * 替换模板文件中的内容
+     * @param str
+     * @param name
+     * @param content
+     * @return
+     */
+    static String replace(String str, String name, String content) {
 
-        p = Pattern.compile("\\{"+name+"\\}");
+        p = Pattern.compile("\\$" + name);
         m = p.matcher(str);
 
         str = m.replaceAll(content);
 
         return str;
     }
+
+
+    public static void main(String[] args) throws Exception {
+
+        String nextHref = "/u283056051/article/details/54614665";
+        while (nextHref != null){
+            Element div = getContentElement(nextHref);
+
+            String title = getTitle(div);
+            System.out.println("************正在下载："+title+"************");
+            String date = getPostDate(div);
+
+            String atricle = getAtricleContent(div);//正文内容
+
+            //System.out.println(atricle);
+            String okContent = replaceRedundancyElement(atricle);
+
+            okContent = handlSpecialChar(okContent);
+
+            nextHref = getNextHref(div); //下一篇文章链接
+
+            create(title,date,okContent,"E:\\template.xml");
+
+            System.out.println("************下载完成："+title+"************");
+            System.out.println();
+        }
+        //System.out.println(handlSpecialCharForTitle("sdf*sdf><d|\\/sdf:fg?hk\""));
+
+    }
+
+
 }
